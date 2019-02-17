4c503899b7f01e29fe7372ea35f83ea9f8d0ceb0
msyksphinz <msyksphinz_dev.gmail.com>
2019-02-18 01:05:13 +0900

    Func: implement hex file load

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+use std::io::{Read, Write, BufReader};
+use std::env;
+
 mod core_base;
 mod riscv_core;
 
@@ -6,8 +10,21 @@ use crate::riscv_core::EnvBase;
 
 use crate::riscv_core::DRAM_BASE;
 
-fn main() {
-
+fn main() -> Result<(), Box<std::error::Error>> {
+    let args: Vec<String> = env::args().collect::<Vec<String>>();
+    
+    if args.len() != 2 {
+        writeln!(std::io::stderr(), "Usage: swimmer_rust_origin hexfile").unwrap();
+        std::process::exit(1);
+    }
+    
+    let file = File::open(&args[1]).unwrap();
+    let filebuf = BufReader::new (file);
+    for result in filebuf.bytes() {
+        let l = result?;
+        println!("{:x}", l);
+    }
+    
     let mut riscv64_core = EnvBase::new();
     for addr in 0..32 {
         riscv64_core.write_memory (addr * 4 + DRAM_BASE, addr as u32);
@@ -20,4 +37,6 @@ fn main() {
         let data = riscv64_core.fetch_memory (addr * 4 + DRAM_BASE);
         println!("Fetch Data = {:08x}", data);
     }
+
+    Ok(())
 }
